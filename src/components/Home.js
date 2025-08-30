fb4e018fca07965d4ae8110d7551ed54c93ec260
Shivakumar Kirigeri <shivakumar641@gmail.com>
2025-08-30 23:45:01 +0530

    task:acual api call in progress

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,14 @@
-import React from "react";
+import axios from "axios";
+import { addlogin, removelogin } from "../store/slices/loginSlice";
+import { SERVER } from "../utils/constants";
+import React, { useEffect } from "react";
 import { useNavigate } from "react-router";
+import { useDispatch, useSelector } from "react-redux";
+import { getgid } from "process";
 
 const Home = () => {
   const navigate = useNavigate();
+  const dispatch = useDispatch();
   return (
     <div>
       <p>Welcome to NoQ</p>
--- src/store/appStore.js
+++ src/store/appStore.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import stationsListReducer from "./slices/stationsListSlice";
+import loginReducer from "./slices/loginSlice";
 
 const appStore = configureStore({
   reducer: {
     stationsList: stationsListReducer,
+    login: loginReducer,
   },
 });
 export default appStore;
