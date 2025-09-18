bb3ed2387fc901994fbaa50bd56b31dd81674678
Shivakumar Kirigeri <shivakumar641@gmail.com>
2025-09-19 00:41:59 +0530

    Fix App.js import and export issue

--- src/app.js
+++ src/app.js
@@ -1,15 +1,7 @@
-import ReactDOM from "react-dom/client";
-import ConfirmedTicketDetails from "./components/ConfirmedTicketDetails";
-import appStore from "./store/appStore";
-import Login from "./components/Login";
-import TicketHistory from "./components/TicketHistory";
-import StationsDetails from "./components/StationsDetails";
-import Payment from "./components/Payment";
-import PassengerDetails from "./components/PassengerDetails";
-import Error from "./components/Error";
 import { Provider } from "react-redux";
-import { createBrowserRouter, Outlet, RouterProvider } from "react-router";
-import Menu from "./components/Menu";
+import appStore from "./store/appStore";
+import { Outlet } from "react-router";
+
 export default function App() {
   return (
     <Provider store={appStore}>
@@ -19,64 +11,3 @@ export default function App() {
     </Provider>
   );
 }
-const appRouter = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    children: [
-      {
-        path: "/",
-        element: <Login />,
-      },
-      {
-        path: "/login",
-        element: <Login />,
-      },
-      {
-        path: "/menu",
-        element: <Menu />,
-      },
-      {
-        path: "/station-details",
-        element: <StationsDetails />,
-      },
-      {
-        path: "/passenger-details",
-        element: <PassengerDetails />,
-      },
-      {
-        path: "/ticket-history",
-        element: <TicketHistory />,
-      },
-      {
-        path: "/confirm-ticket",
-        element: <ConfirmedTicketDetails />,
-      },
-      {
-        path: "/payment",
-        element: <Payment />,
-      },
-      /*
-      {
-        path: "/reviewbooking",
-        element: <ReviewBooking />,
-      },
-      {
-        path: "/confirmticket",
-        element: <ConfirmTicket />,
-      },
-      {
-        path: "/my-ticket-history",
-        element: <TicketHistory />,
-      },*/
-      ,
-      ,
-    ],
-    errorElement: <Error />,
-  },
-]);
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <RouterProvider router={appRouter}>
-    <App />
-  </RouterProvider>
-);
